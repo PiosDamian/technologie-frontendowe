c719b25b5558bd12d0329490befcb2a9ab637de5
PiosDamian <piosdamian@gmail.com>
2021-02-25 22:15:27 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
+    /**
+    * używam proxy tak, aby aplikacja łączyła się w ten sam sposób z backend deweloperskim i produkcyjnym
+    * unikam dzięki temu konieczności konfigurowania CORS, nie mam problemu ze zmianą protokołu (jeżeli na przykład backend stoi na https),
+    * przeglądarka nie wysyła żądań typu Options(pre-flights) jeżeli dodamy nagłówek np. `Authentication`
+    * w aplikacji mogę używać url relatywnych, nie martwiąc się o domenę backend - tak jak to wygląda w aplikacji produkcyjnej - zamiast http://localhost:8000/todos/ - todos/
+    */
     proxy: {
       '/*': {
         target: 'http://localhost:8000/'
