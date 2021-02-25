9938797de8ca64ea1549dce860f65f8884a67aac
PiosDamian <piosdamian@gmail.com>
2021-02-25 22:16:47 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ module.exports = {
     * unikam dzięki temu konieczności konfigurowania CORS, nie mam problemu ze zmianą protokołu (jeżeli na przykład backend stoi na https),
     * przeglądarka nie wysyła żądań typu Options(pre-flights) jeżeli dodamy nagłówek np. `Authentication`
     * w aplikacji mogę używać url relatywnych, nie martwiąc się o domenę backend - tak jak to wygląda w aplikacji produkcyjnej - zamiast http://localhost:8000/todos/ - todos/
+    * używając proxy pracujemy z aplikcją w taki sam sposób jak byśmy mieli działającą aplikację produkcyjną
     */
     proxy: {
       '/*': {
